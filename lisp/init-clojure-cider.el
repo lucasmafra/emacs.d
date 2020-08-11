eed153d1209601b93d9095078d3a409996b4e780
Lucas Mafra <lucas.mafra95@gmail.com>
2020-08-11 09:03:37 -0300

    [clojure] enable paredit

--- lisp/init-clojure-cider.el
+++ lisp/init-clojure-cider.el
@@ -3,13 +3,6 @@
 ;;; Code:
 
 (require 'init-clojure)
-(require-package 'clj-refactor)
-
-(defun clojure-refactor-setup ()
-  (clj-refactor-mode 1)
-  (yas-minor-mode 1) ;; for adding require/use/import statements
-  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-  (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (when (maybe-require-package 'cider)
   (setq nrepl-popup-stacktraces nil)
@@ -23,9 +16,6 @@
     (with-eval-after-load 'cider
       (with-eval-after-load 'flycheck
         (flycheck-clojure-setup)))))
-
-
-(add-hook 'clojure-mode-hook #'clojure-refactor-setup)
 
 (provide 'init-clojure-cider)
 ;;; init-clojure-cider.el ends here
--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -3,6 +3,13 @@
 ;;; Code:
 
 ;; See also init-clojure-cider.el
+(require-package 'clj-refactor)
+
+(defun clojure-refactor-setup ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ;; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (when (maybe-require-package 'clojure-mode)
   (require-package 'cljsbuild-mode)
@@ -10,8 +17,10 @@
 
   (with-eval-after-load 'clojure-mode
     (add-hook 'clojure-mode-hook 'sanityinc/lisp-setup)
-    (add-hook 'clojure-mode-hook 'subword-mode)))
+    (add-hook 'clojure-mode-hook 'subword-mode)
+    (add-hook 'clojure-mode-hook 'enable-paredit-mode)))
 
+(add-hook 'clojure-mode-hook #'clojure-refactor-setup)
 
 (provide 'init-clojure)
 ;;; init-clojure.el ends here
